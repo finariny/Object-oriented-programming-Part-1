fc473237a79c1f3d3d1d9ba961dbf8393f304d93
Anastasia <nastay.dr2002@gmail.com>
2022-12-28 19:49:19 +0400

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car lada = new Car("Lada", "Granta", 1.7, "желтый", 2015, "Россия");
+
+        System.out.println(lada);
+
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020,
+                "Германия");
+
+        System.out.println(audi);
+
+        Car bmw = new Car("BMW", "Z8", 3.0, "чёрный", 2021, "Германия");
+
+        System.out.println(bmw);
+
+        Car kia = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018,
+                "Южная Корея");
+
+        System.out.println(kia);
+
+        Car hyundai = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016,
+                "Южная Корея");
+
+        System.out.println(hyundai);
     }
 }
